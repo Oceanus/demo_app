62998e96102b967181e6ba7d4f7d7e689f56f01e
Kristina Shindyapin <kristina.shindyapin@gmail.com>
2013-04-18 17:50:27 -0400

    Added a Delete Button, Auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
-	def create
+   http_basic_authenticate_with :name => "foo", :password => "secret", :only => :destroy
+
+  def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
   end
+
+  def destroy
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.find(params[:id])
+  	@comment.destroy
+  	redirect_to post_path(@post)
+  end
+
 end
